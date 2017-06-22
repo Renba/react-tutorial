9bb50071da34cd3bca7f51d20f3d966cb52bbc15
renba <cs_abner@hotmail.com>
2017-06-22 16:59:10 -0500

    forms

--- App.jsx
+++ App.jsx
@@ -1,27 +1,39 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-  constructor() {
-    super();
-    this.findDomNodeHandler = this.findDomNodeHandler.bind(this)
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: 'Initial data ...'
+    }
+    this.updateState = this.updateState.bind(this);
+  }
 
-  findDomNodeHandler(){
-    var myDiv= document.getElementById('myDiv');
-    ReactDOM.findDOMNode(myDiv).style.color = 'red';
-  };
-  render() {
-    return (
+  updateState(e){
+    this.setState({ data: e.target.value });
+  }
+
+  render(){
+    return(
       <div>
-        <button onClick={this.findDomNodeHandler}>
-          FIND DOME NODE
-        </button>
-        <div id= "myDiv">NODE</div>
+        <Content myDataProp = {this.state.data} updateStateProp = {this.updateState}>
+        </Content>
       </div>
     );
   }
+}
 
+class Content extends React.Component {
+  render() {
+    return (
+      <div>
+        <input type = "text" value={this.props.myDataProp}
+          onChange = {this.props.updateStateProp}
+          />
+        <h3>{this.props.myDataProp}</h3>
+      </div>
+    );
+  }
 }
 
 export default App;
